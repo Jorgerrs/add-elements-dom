d005baee8f043c027fb3480df499cb912623b546
Jorge <jordixl90@gmail.com>
2025-03-09 16:35:27 +0100

    resolucion ejercicio 2 sprint 3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
  // Aquí tu código
+const addElementButton = document.getElementById('agregar');
+const itemList = document.getElementById('lista');
+
+addElementButton.addEventListener('click',function(){
+    const newItem = prompt('agregar elemento');
+    
+    const listItem = document.createElement('li') 
+    listItem.textContent = newItem;
+    itemList.appendChild(listItem);
+
+});
+
